9e02bc2f5cf2fa91edc1c33bc6b4e0aeb7f9a490
noudas <jonath.kaha@gmail.com>
2025-05-27 19:47:27 +0300

    Adding Skillscomponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import './App.css'
 import SkillsComponent from './components/skillsComponent/skillsComponent'
 import TitleComponent from './components/titleComponent/titleComponent'
+import Arrow from './assets/Arrow.svg'
 
 function App() {
 
   return (
-    <>
+    <div className='app'>
       <TitleComponent />
-      <div>
+      <div className='skills-component-container'>
         <SkillsComponent direction='left' title='Front End' skills={['REACT','TYPESCRIPT','JAVASCRIPT','HTML','CSS','TAILWIND', 'BOOTSTRAP']} />
+        <img src={Arrow} alt="Arrow" className='arrow' />
         <SkillsComponent direction='right' title='Back End' skills={['PHP','SQL','POSTGRES','PYTHON','NODE','KOTLIN','UML','MONGODB']} />
       </div>
       
-    </>
+    </div>
   )
 }
 
