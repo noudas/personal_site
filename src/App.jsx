4ba0f5861b6adb4f7dd4b51080390a55ea7e5105
noudas <jonath.kaha@gmail.com>
2025-05-27 19:24:30 +0300

    skills component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import SkillsComponent from './components/skillsComponent/skillsComponent'
 import TitleComponent from './components/titleComponent/titleComponent'
 
 function App() {
@@ -6,6 +7,10 @@ function App() {
   return (
     <>
       <TitleComponent />
+      <div>
+        <SkillsComponent direction='left' title='Front End' skills={['REACT','TYPESCRIPT','JAVASCRIPT','HTML','CSS','TAILWIND', 'BOOTSTRAP']} />
+        <SkillsComponent direction='right' title='Back End' skills={['PHP','SQL','POSTGRES','PYTHON','NODE','KOTLIN','UML','MONGODB']} />
+      </div>
       
     </>
   )
--- src/components/skillsComponent/skillsComponent.jsx
+++ src/components/skillsComponent/skillsComponent.jsx
@@ -1,12 +1,14 @@
+import "./skillsComponent.css";
+
 const SkillsComponent = ({ direction = 'left', title = 'Front End', skills = [] }) => {
     return (
         <div className={`skills-container ${direction}`}>
             <h2 className="skills-title">{title}</h2>
-            <ul className="skills-list">
+            <div className="skills-list">
                 {skills.map((skill, index) => (
-                    <li key={index} className="skill-item">{skill}</li>
+                    <div key={index} className="skill-item">{skill}</div>
                 ))}
-            </ul>
+            </div>
         </div>
     );
 };
