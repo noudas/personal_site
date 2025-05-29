5b6481c37a72e031c24fbf0dc114381b9628bd69
noudas <jonath.kaha@gmail.com>
2025-05-29 10:37:46 +0300

    Added audios

--- src/components/hireMeComponent/hireMeComponent.jsx
+++ src/components/hireMeComponent/hireMeComponent.jsx
@@ -2,11 +2,20 @@ import { useState, useEffect } from "react";
 import Confetti from "react-confetti";
 import "./hireMeComponent.css";
 import Pointer from "../../assets/Pointer.svg";
+import Crowd from "../../assets/crowd.mp3";
+import Yay from "../../assets/yay.mp3";
+import Victory from "../../assets/Victory.mp3";
 
 const HireMeComponent = () => {
     const [showConfetti, setShowConfetti] = useState(false);
 
     const handleClick = () => {
+        const crowdAudio = new Audio(Crowd);
+        const yayAudio = new Audio(Yay);
+        const victoryAudio = new Audio(Victory);
+        victoryAudio.play();
+        crowdAudio.play();
+        yayAudio.play();
         setShowConfetti(true);
 
         // Delay before redirecting to LinkedIn
