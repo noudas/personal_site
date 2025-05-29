4e03ff2716484d8d03b31d91d48397624f126955
noudas <jonath.kaha@gmail.com>
2025-05-29 15:51:56 +0300

    Creating pages

---
+++ src/components/projectComponent/projectComponent.jsx
@@ -0,0 +1,22 @@
+import './projectComponent.css';
+import Arrow from '../../assets/Arrow.svg';
+
+
+const ProjectComponent = ({ image, title, description, link }) => {
+  return (
+    <div className="project-card">
+      <img src={image} alt={title} className="project-image" />
+
+      <div className="project-description">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+
+        <a href={link} className="arrow-button" target="_blank" rel="noopener noreferrer">
+        <img src={Arrow} alt="Arrow" className="arrow-icon" />
+        </a>
+    </div>
+  );
+};
+
+export default ProjectComponent;
---
+++ src/pages/hireMe/HireMe.jsx
@@ -0,0 +1,44 @@
+// HireMe.jsx
+import { useEffect, useState } from 'react'
+import './HireMe.css'
+import SkillsComponent from '../../components/skillsComponent/skillsComponent'
+import TitleComponent from '../../components/titleComponent/titleComponent'
+import Arrow from '../../assets/Arrow.svg'
+import ThikkArrow from '../../assets/ThikkArrow.svg'
+import HireMeComponent from '../../components/hireMeComponent/hireMeComponent'
+
+function HireMe() {
+  const [isMobile, setIsMobile] = useState(false)
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 900)
+    }
+
+    handleResize() // Initial check
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
+  return (
+    <div className='hire-me-page'>
+      <TitleComponent />
+      <div className='skills-component-container'>
+        <SkillsComponent
+          direction='left'
+          title='Front End'
+          skills={['REACT', 'TYPESCRIPT', 'JAVASCRIPT', 'HTML', 'CSS', 'TAILWIND', 'BOOTSTRAP']}
+        />
+        <img src={isMobile ? ThikkArrow : Arrow} alt='Arrow' className='arrow' />
+        <SkillsComponent
+          direction='right'
+          title='Back End'
+          skills={['PHP', 'SQL', 'POSTGRES', 'PYTHON', 'NODE', 'KOTLIN', 'UML', 'MONGODB', 'EXPRESS']}
+        />
+      </div>
+      <HireMeComponent />
+    </div>
+  )
+}
+
+export default HireMe
