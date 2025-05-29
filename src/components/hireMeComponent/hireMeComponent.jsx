fe816e7c3a585a4962f3d3fec2b295c97a039ed2
noudas <jonath.kaha@gmail.com>
2025-05-29 10:27:24 +0300

    Confetti add

--- src/components/hireMeComponent/hireMeComponent.jsx
+++ src/components/hireMeComponent/hireMeComponent.jsx
@@ -1,17 +1,31 @@
+import { useState, useEffect } from "react";
+import Confetti from "react-confetti";
 import "./hireMeComponent.css";
 import Pointer from "../../assets/Pointer.svg";
 
 const HireMeComponent = () => {
+    const [showConfetti, setShowConfetti] = useState(false);
+
+    const handleClick = () => {
+        setShowConfetti(true);
+
+        // Delay before redirecting to LinkedIn
+        setTimeout(() => {
+            window.open("https://www.linkedin.com/in/jonathankah/", "_blank");
+        }, 1500); // 1.5 seconds for confetti
+    };
+
     return (
         <>
-            <div className="HireMeComponent">
+            {showConfetti && <Confetti />}
+            <div className="HireMeComponent" onClick={handleClick} style={{ cursor: "pointer" }}>
                 <div className="hireme">
                     HIRE ME!
                 </div>
-            <img src={Pointer} alt="Pointer" className="pointer" />
+                <img src={Pointer} alt="Pointer" className="pointer" />
             </div>
         </>
-    )
-}
+    );
+};
 
 export default HireMeComponent;
