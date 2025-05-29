a53917087902f35a62f2a9db7521c620145f0a24
noudas <jonath.kaha@gmail.com>
2025-05-29 12:02:14 +0300

    Fixing css

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,34 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import SkillsComponent from './components/skillsComponent/skillsComponent'
 import TitleComponent from './components/titleComponent/titleComponent'
 import Arrow from './assets/Arrow.svg'
+import ThikkArrow from './assets/ThikkArrow.svg'
 import HireMeComponent from './components/hireMeComponent/hireMeComponent'
 
 function App() {
+  const [isMobile, setIsMobile] = useState(false)
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 900)
+    }
+
+    handleResize() // Initial check
+    window.addEventListener('resize', handleResize)
+
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
 
   return (
     <div className='app'>
       <TitleComponent />
       <div className='skills-component-container'>
         <SkillsComponent direction='left' title='Front End' skills={['REACT','TYPESCRIPT','JAVASCRIPT','HTML','CSS','TAILWIND', 'BOOTSTRAP']} />
-        <img src={Arrow} alt="Arrow" className='arrow' />
+        <img src={isMobile ? ThikkArrow : Arrow} alt="Arrow" className='arrow' />
         <SkillsComponent direction='right' title='Back End' skills={['PHP','SQL','POSTGRES','PYTHON','NODE','KOTLIN','UML','MONGODB',"EXPRESS"]} />
       </div>
-        <HireMeComponent />
+      <HireMeComponent />
     </div>
   )
 }
