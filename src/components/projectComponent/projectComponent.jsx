8d347d7e02781b86c871528405cbb528f7d04bef
noudas <jonath.kaha@gmail.com>
2025-06-03 14:49:31 +0300

    adding logos

--- src/components/projectComponent/projectComponent.jsx
+++ src/components/projectComponent/projectComponent.jsx
@@ -1,5 +1,11 @@
 import './projectComponent.css';
 import Arrow from '../../assets/BiggestArrowest.svg';
+import ReactLogo from '../../assets/logos/react.svg';
+import TypescriptLogo from '../../assets/logos/typescript.svg';
+import MongoLogo from '../../assets/logos/mongo.svg';
+import MysqlLogo from '../../assets/logos/mysql.svg';
+import NodeLogo from '../../assets/logos/node.svg';
+import PythonLogo from '../../assets/logos/python.svg';
 
 
 const ProjectComponent = ({ title, description, link }) => {
@@ -10,7 +16,7 @@ const ProjectComponent = ({ title, description, link }) => {
         <h1>{title}</h1>
         <p>{description}</p>
         <h2>TECHS:</h2>
-        <ul>
+        <ul className='project-techs'>
           <li>React</li>
           <li>Node.js</li>
           <li>Express</li>
@@ -19,9 +25,12 @@ const ProjectComponent = ({ title, description, link }) => {
         </ul>
       </div>
 
+    <div className='project-arrow'>
         <a href={link} className="arrow-button" target="_blank" rel="noopener noreferrer">
             <img src={Arrow} alt="Arrow" className="arrow-icon" />
         </a>
+    </div>
+
     </div>
   );
 };
--- src/pages/projects/projects.jsx
+++ src/pages/projects/projects.jsx
@@ -1,11 +1,12 @@
 import ProjectComponent from "../../components/projectComponent/projectComponent";
 import TitleComponent from "../../components/titleComponent/titleComponent";
+import "./projects.css";
 
 const Projects = () =>{
     return (
         <>
             <TitleComponent title={"PROJECTS"} color={"var(--azulClaro)"}/>
-            <ProjectComponent description={"Hard cheese cheesecake jarlsberg. Emmental cheesy feet taleggio red leicester cheeseburger mozzarella ricotta cheesy feet."} title={"PROJECT NAME AQUI"} link={""} />
+            <ProjectComponent description={"Hard cheese cheesecake jarlsberg. Emmental cheesy feet taleggio red leicester cheeseburger mozzarella ricotta cheesy feet. Hard cheese cheesecake jarlsberg. Emmental cheesy feet taleggio red leicester cheeseburger mozzarella ricotta cheesy feet."} title={"PROJECT NAME AQUI"} link={""} />
         </>
     )
 }
