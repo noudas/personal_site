e4f0e3ba4cfb26878a1f41a46b98721cceed9ebb
noudas <jonath.kaha@gmail.com>
2025-06-04 15:42:22 +0300

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import './App.css'
-import HireMe from './pages/hireMe/HireMe'
-import Projects from './pages/projects/projects'
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HireMe from './pages/hireMe/HireMe';
+import Projects from './pages/projects/projects';
 
 function App() {
-
   return (
-    <div className='app'> 
-        <Projects />
-
+    <div className="app">
+      <Router>
+        <Routes>
+          <Route path="/" element={<HireMe />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
